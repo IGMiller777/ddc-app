e64dd322b2819e96af5da1bd3ece3c292f95b00f
Ivan Gamanovich <igamanovich@icloud.com>
2024-09-24 12:57:58 +0300

    feat: update with loader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SideBarComponent } from './core/components/side-bar/side-bar.component';
-import { EditorPanelComponent } from './core/components/editor-panel/editor-panel.component';
-import { BaseLayoutComponent } from './core/components/base-layout/base-layout.component';
+import { BaseLayoutComponent } from '@components/base-layout/base-layout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, BaseLayoutComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'drag-copy';
